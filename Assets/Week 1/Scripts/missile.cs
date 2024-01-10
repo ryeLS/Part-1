5ea3c73714d7ccd81a0e16a70381e28be4f737e2
luesue <luesue@sheridancollege.ca>
2024-01-10 17:26:49 -0500

    task 4a bullets move

---
+++ Assets/Week 1/Scripts/missile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+    }
+}
