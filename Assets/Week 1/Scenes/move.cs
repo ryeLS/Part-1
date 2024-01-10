eabb529feeb78f9c51b73a116b004fe351366482
luesue <luesue@sheridancollege.ca>
2024-01-10 15:47:05 -0500

    task 2b finished

--- Assets/Week 1/Scenes/move.cs
+++ Assets/Week 1/Scenes/move.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float speed = 3f;
+    public GameObject missilePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,9 @@ public class move : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missilePrefab, spawn.position, spawn.rotation);
+        }
     }
 }
